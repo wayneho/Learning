f42df5ef1e6a50e0c4e4c56088c23500784067ad
Wayne Ho <wayneho92@hotmail.com>
2015-08-14 22:12:32 -0700

    Tagging feature completed

---
+++ Project Rails/blogger/app/controllers/tags_controller.rb
@@ -0,0 +1,13 @@
+class TagsController < ApplicationController
+	def show
+		@tag = Tag.find(params[:id])
+	end
+	def index
+		@tag = Tag.all
+	end
+	def destroy
+		tag = Tag.find(params[:id])
+		tag.destroy
+		redirect_to tags_path
+	end
+end
